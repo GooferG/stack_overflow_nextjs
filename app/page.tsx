a17f3355f838710ea791f5aae98c49f7f0ba5f60
Luiz Meneghim <luiz.meneghim@hotmail.com>
2023-12-06 19:54:26 -0700

    added auth routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1 className="h1-bold">Next.js 13 we&apos;re coming!</h1>
+      <div className="primary">
+        <h1 className="text-xs text-white">Next.js 13 we&apos;re coming!</h1>
         <h2 className="h2-bold">Next.js 13 we&apos;re coming!</h2>
-        <h3 className="h3-bold">Next.js 13 we&apos;re coming!</h3>
+        <h3 className="text-green-500">Next.js 13 we&apos;re coming!</h3>
       </div>
     </main>
   );
